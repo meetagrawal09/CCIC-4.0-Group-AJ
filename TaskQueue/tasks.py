50f008446f0ce5ade9a13fe66fafb73e0eef0629
meetagrawal09 <agrawalmeet91@gmail.com>
2022-12-29 19:18:16 +0530

    added get csv to a folder

--- TaskQueue/tasks.py
+++ TaskQueue/tasks.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-def get_date():
+def get_date_csv():
     date = datetime.datetime.now().strftime("%d%m%Y")
     date = date[:4]+date[6:]
     return date
@@ -15,10 +15,13 @@ def collect_news_data():
     texts = [div.text for div in divs]
     return texts
 
-def get_csv():
-    url = 'https://www1.nseindia.com/archives/equities/corpbond/corpbond'+get_date()+'.csv'
+def get_csv(url_name, file_name):
+    url = url_name+get_date_csv()+'.csv'
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    print(soup.prettify())
+    url_content = response.content
+    csv_file = open('./data/'+file_name, 'wb')
+    csv_file.write(url_content)
+    csv_file.close()
 
-get_csv()
+
+get_csv('https://www1.nseindia.com/archives/equities/corpbond/corpbond', 'corpbond.csv')
