fa8ee02c22c78b682cc8700beda3557e7f7c0b34
meetagrawal09 <agrawalmeet91@gmail.com>
2022-12-31 10:53:16 +0530

    added build reports

--- TaskQueue/build_report.py
+++ TaskQueue/build_report.py
@@ -1,29 +1,147 @@
 from fpdf import FPDF
-from tasks import collect_news_data
+from tasks import collect_news_data, opt_flow, insti_flow, market_to
 
-def build_table_section(data, pdf):
-    page_width = pdf.w
-    cell_width = page_width / 3
-    html = '<table style="border-collapse: collapse; width: 100%;">'
-    for row in data:
-        html += '<tr>'
-        for cell in row:
-            html += f'<td style="border: 1px solid black; width: {cell_width}px;">{cell}</td>'
-        html += '</tr>'
-    html += '</table>'
-    pdf.set_xy(10, 10)
-    pdf.write_html(html, ln=True)
-
-def build_news_section(pdf, data):
+def build_news_section(pdf, news_data):
 
     pdf.write_html("""
         <font color="#0000ff"><p>***Market Commentary - Intended for Institutional Clients Only***</p></font>
         <h5><b>NEWS</b></h5>
     """)
     pdf.set_font('helvetica','',10)
-    for element in data:
+    for element in news_data:
         pdf.multi_cell(0, 5, element)
         pdf.ln()
+
+    pdf.write_html("""
+        <h5><b>MARKET TURNOVER</b></h5>
+    """)
+
+    market_turnover_data = market_to()
+
+    pdf.write_html(
+        f"""<table border="1">
+                <thead>
+                    <tr>
+                        <th width="50%">Category</th>
+                        <th width="50%">{market_turnover_data[3]}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Stock Future</td>
+                        <td>{market_turnover_data[0]}</td>
+                    </tr>
+                    <tr>
+                        <td>Nifty</td>
+                        <td>{market_turnover_data[1]}</td>
+                    </tr>
+                    <tr>
+                        <td>BankNifty</td>
+                        <td>{market_turnover_data[2]}</td>
+                    </tr>
+                </tbody>
+            </table>""",
+        table_line_separators=True,
+    )
+
+
+
+    pdf.write_html("""
+        <h5><b>INSTITUTIONAL FLOW</b></h5>
+    """)
+
+    i_flow_data = insti_flow()
+
+    pdf.write_html(
+        f"""<table border="1">
+                <thead>
+                    <tr>
+                        <th width="33%">Category</th>
+                        <th width="33%">{i_flow_data[12]}</th>
+                        <th width="33%">{i_flow_data[13]}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>FPI Cash</td>
+                        <td>{i_flow_data[0]}</td>
+                        <td>{i_flow_data[6]}</td>
+                    </tr>
+                    <tr>
+                        <td>FPI Index future value</td>
+                        <td>{i_flow_data[1]}</td>
+                        <td>{i_flow_data[7]}</td>
+                    </tr>
+                    <tr>
+                        <td>FPI stock future value</td>
+                        <td>{i_flow_data[2]}</td>
+                        <td>{i_flow_data[8]}</td>
+                    </tr>
+                    <tr>
+                        <td>DII stock future value</td>
+                        <td>{i_flow_data[3]}</td>
+                        <td>{i_flow_data[9]}</td>
+                    </tr>
+                    <tr>
+                        <td>Dii index future value</td>
+                        <td>{i_flow_data[4]}</td>
+                        <td>{i_flow_data[10]}</td>
+                    </tr>
+                    <tr>
+                        <td>OI PCR</td>
+                        <td>{i_flow_data[5]}</td>
+                        <td>{i_flow_data[11]}</td>
+                    </tr>
+                </tbody>
+            </table>""",
+        table_line_separators=True,
+    )
+
+    pdf.write_html("""
+        <h5><b>OPTION FLOWS</b></h5>
+    """)
+
+    opt_flow_data = opt_flow()
+
+    pdf.write_html(
+        f"""<table border="1">
+                <thead>
+                    <tr>
+                        <th width="25%">Category</th>
+                        <th width="25%">{opt_flow_data[8]}</th>
+                        <th width="25%">{opt_flow_data[9]}</th>
+                        <th width="25%">1D Change</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>FPI call Option</td>
+                        <td>{opt_flow_data[0]}</td>
+                        <td>{opt_flow_data[4]}</td>
+                        <td>{abs(opt_flow_data[0]-opt_flow_data[4])}</td>
+                    </tr>
+                    <tr>
+                        <td>FPI put Option</td>
+                        <td>{opt_flow_data[1]}</td>
+                        <td>{opt_flow_data[5]}</td>
+                        <td>{abs(opt_flow_data[1]-opt_flow_data[5])}</td>
+                    </tr>
+                    <tr>
+                        <td>DII call Option</td>
+                        <td>{opt_flow_data[2]}</td>
+                        <td>{opt_flow_data[6]}</td>
+                        <td>{abs(opt_flow_data[2]-opt_flow_data[6])}</td>
+                    </tr>
+                    <tr>
+                        <td>DII put Option</td>
+                        <td>{opt_flow_data[3]}</td>
+                        <td>{opt_flow_data[7]}</td>
+                        <td>{abs(opt_flow_data[3]-opt_flow_data[7])}</td>
+                    </tr>
+                </tbody>
+            </table>""",
+        table_line_separators=True,
+    )
 
 def build_reports(report_name):
     pdf = FPDF()
